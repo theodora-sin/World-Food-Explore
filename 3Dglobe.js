7bff28e938cc84e2b58156d7820e26d02794b3be
theodora-sin <theodrasin@gmail.com>
2026-07-30 12:21:14 +0100

    Update 3Dglobe.js

--- 3Dglobe.js
+++ 3Dglobe.js
@@ -46,7 +46,7 @@ const cityData=[
   name:"Cebu ",
   country:"Phillippines",
   lat:"10.296785",
-  ltn:"123.915569",
+  lng:"123.915569",
   cuisines:[{
     type:"Filipino",
     dish_name:"Lechon",
