b418d5a2d52d75d3ce9ad32382df7133a8015c9f
theodora-sin <theodrasin@gmail.com>
2026-07-30 18:32:41 +0100

    Create cityData.js

---
+++ cityData.js
@@ -0,0 +1,157 @@
+export const cityData=[
+  {
+    name:"Bangkok",
+    country:"Thailand",
+    lat:13.806424,
+    lng:100.448832,
+    cuisines:[{
+      type: "Thai",
+      dish_name:"Pad Thai",
+      course:"main",
+      photoURL:"images/pad_thai.jpg",
+      description:"Stir-fried rice noodles with egg, fresh mung bean sprouts, chopped scallions or garlic chives, tamarind, cubed firm tofu.",
+      recommendations:[
+        {
+          name:"Thipsamai",
+          priceRange:"$$",
+          address:"313, 315 Maha Chai Rd, Samran Rat, Phra Nakhon, Bangkok 10200, Thailand",
+          link:"https://www.google.com/maps/place/Thipsamai+Padthai+Pratoopee/@13.7554233,100.4028562,11z/data=!4m15!1m8!3m7!1s0x30e2991678561141:0x8549dd651a717306!2s313+Maha+Chai+Rd,+Khwaeng+Samran+Rat,+Khet+Phra+Nakhon,+Krung+Thep+Maha+Nakhon+10200,+Thailand!3b1!8m2!3d13.75275!4d100.5048336!16s%2Fg%2F11snqxdnj_!3m5!1s0x30e2991678584ec5:0x698c069655046fbe!8m2!3d13.7527976!4d100.504823!16s%2Fg%2F1tksf77l?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+        }
+      ]
+    }]
+  },
+{
+  name:"Da Nang",
+  country:"Vietnam",
+  lat:16.090693,
+  lng:108.409756,
+  cuisines:[{
+    type:"Vietnamese",
+    dish_name:"Bún Chả Cá",
+    course:"main",
+    photoURL:"images/fishcake.jpg",
+    description:"Fish cake soup with seasoned crab mince, round rice noodles and a fragrant broth.",
+    recommendations:[{
+      name:"Bun Cha Ca Ba Phien",
+      priceRange:"$",
+      address: "06 Đường Cao Hồng Lãnh, Hội An, Đà Nẵng, Vietnam",
+      link:"https://www.google.com/maps/place/B%C3%BAn+Ch%E1%BA%A3+C%C3%A1+B%C3%A0+Phi%E1%BA%BFn/@15.878058,108.312044,13.8z/data=!4m10!1m2!2m1!1sBun+Cha+Ca+Ba+Phien!3m6!1s0x31420e7ce57f5553:0x8dffd5d421e654c!8m2!3d15.8779342!4d108.3228283!15sChNCdW4gQ2hhIENhIEJhIFBoaWVuWhUiE2J1biBjaGEgY2EgYmEgcGhpZW6SARBhc2lhbl9yZXN0YXVyYW50mgFEQ2k5RFFVbFJRVU52WkVOb2RIbGpSamx2VDJ4c1JWWlZWakpXYTNnelQxaG9ibU5yVWtWUFIwNVdZMGhDWVdGSVl4QULgAQD6AQQIABBJ!16s%2Fg%2F11hd1lp12h?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+    }
+    ]
+  }]
+},
+{
+  name:"Cebu ",
+  country:"Phillippines",
+  lat:10.296785,
+  lng:123.915569,
+  cuisines:[{
+    type:"Filipino",
+    dish_name:"Lechon",
+    course:"main",
+    photoURL:"images/lechon.jpg",
+    description:"A whole pig slowly roasted over charcoal, known for its crunchy skin and soft, juice meat.",
+    recommendations:[{
+      name:"CNT Lechon",
+      priceRange:"$",
+      address:"8W59+W36, Jose L Briones Street, Cebu City, 6000 Lalawigan ng Cebu, Philippines",
+      link:"https://www.google.com/maps/place/CNT+Lechon+-+SM+Cebu+Across/@10.3097313,123.9176729,13z/data=!4m6!3m5!1s0x33a9996ddc391bfb:0xdbd469bdf299cc16!8m2!3d10.3097778!4d123.9176463!16s%2Fg%2F11hd9r4lnq?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+    }]
+  }]
+},
+{
+  name:"Taipei",
+  country:"Taiwan",
+  lat:25.033968,
+  lng:121.568353,
+  cuisines:[{
+    type:"Taiwanese",
+    dish_name:"Lu Rou Fan",
+    course:"main",
+    photoURL:"images/lurofan.webp",
+    description:"Savory braised pork rice with a layer of rich, gelantinous pork belly over steamed rice.",
+    recommendations:[{
+      name:"Dadaocheng Luroufan",
+      priceRange:"$",
+      address:"No. 17號, Lane 220, Chang'an W Rd, Jianming Village, Datong District, Taipei City, Taiwan 103",
+      link:"https://www.google.com/maps/place/%E5%A4%A7%E7%A8%BB%E5%9F%95%E9%AD%AF%E8%82%89%E9%A3%AF/@25.0503594,121.5140602,18.06z/data=!4m6!3m5!1s0x3442a96d2ecfd72b:0x21c4c5b2853cd189!8m2!3d25.0509665!4d121.5146457!16s%2Fg%2F11dzsx6s59?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+    }]
+  }]
+},
+{
+  name:"HongKong",
+  country:"",
+  lat:22.303117,
+  lng:114.100703,
+  cuisines:[{
+    type:"Cantonese",
+    dish_name:"Dim Sum",
+    course:"brunch",
+    photoURL:"images/dimsum.jpg",
+    description:"A traditional Chinese style of brunch, featuring small, bite-sized dishes served in bamboo steamer baskets or on small plates alongside with hot tea.",
+    recommendations:[{
+      name:"Lin Heung Tea House",
+      priceRange:"$$",
+      address:"Hong Kong, Tsim Sha Tsui, Kimberley Rd, 25號1-2/F",
+      link:"https://www.google.com/maps/place/Lin+Heung+Lau.+TST/data=!4m2!3m1!1s0x0:0x8370d6550569649e?sa=X&ved=1t:2428&ictx=111"
+    }]
+  }]
+  },
+  {
+    name:"Macao",
+    country:"",
+    lat:22.193716,
+    lng:113.538120,
+    cuisines:[{
+      type:"Portuguese and Macanese",
+      dish_name:"Portuguese egg tarts",
+      course:"dessert",
+      photoURL:"images/portuguese.jpg",
+      description:"A pastry featuring a blistered, carmaelized custard filling inside a crisp, flaky pastry shell.",
+      recommendations:[{
+        name:"Lord Stow's Bakery",
+        priceRange:"$",
+        address:"1 Rua do Tassara, Coloane Town Square, Macau",
+        link:"https://www.google.com/maps/place/Lord+Stow's+Bakery+Main+Store/@22.1256567,113.5440593,15.15z/data=!4m10!1m2!2m1!1slord+stow's+bakery!3m6!1s0x34017035b7ea09d9:0x321981ba17a4b09e!8m2!3d22.1183139!4d113.5511155!15sChJsb3JkIHN0b3cncyBiYWtlcnkiA4gBAVoUIhJsb3JkIHN0b3cncyBiYWtlcnmSAQZiYWtlcnmaASNDaFpEU1VoTk1HOW5TMFZKUTBGblNVTjRhbkJZYWxCM0VBReABAPoBBAgjEEw!16s%2Fg%2F11df0rfff7!5m1!1e1?entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+      }]
+    }]
+  },
+  {
+    name:"Beijing",
+    country:"China",
+    lat:40.025568,
+    lng:117.236542,
+    cuisines:[{
+      type:"Chinese",
+      dish_name:"Peking Duck:",
+      course:"Main",
+      photoURL:"images/pekingduck.jpg",
+      description:"Duck has been roasted until the skin is crisp and golden, sliced tableside and wrapped in thin pancakes with sweet bean paste and scallions.",
+      recommendations:[{
+        name:"Siji Minfu",
+        priceRange:"$$",
+        address:"32 Dengshikou W St, Dongcheng, Beijing, China, 100006",
+        link:"https://www.google.com/maps/place/Siji+Minfu/@39.9481549,116.3483809,12z/data=!4m10!1m2!2m1!1ssiji+minfu!3m6!1s0x35f052d0684291c3:0xa8ed700f63aefeaa!8m2!3d39.9182472!4d116.4088821!15sCgpzaWppIG1pbmZ1WgwiCnNpamkgbWluZnWSARJjaGluZXNlX3Jlc3RhdXJhbnTgAQA!16s%2Fg%2F1thspk95!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+      }]
+    }]
+  },
+  {
+    name:"Shanghai",
+    country:"China",
+    lat:31.227199,
+    lng:121.830919,
+    cuisines:[{
+        type:"Chinese",
+        dish_name:"Sheng Jian Bao",
+        course:"Breakfast",
+        photoURL:"images/dumplings.jpg",
+        description:"Shanghai pan-fried pork buns with a crispy golden bottom, a soft fluffy yeast-dough and a juicy meat filling.",
+        recommendations:[{
+            name:"Yang's Fried-Dumpling",
+            priceRange:"$",
+            address:"Hongyi Plaza, 1层299 Nanjing Rd (E), 299, Huangpu, Shanghai, China, 200002",
+            link:"https://www.google.com/maps/search/Xiao+Yang+Sheng+Jian/@30.7568317,113.3448898,5z/data=!3m1!4b1!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+        }]    
+  }]
+  }
+]
