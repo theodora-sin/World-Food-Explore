d3220ed1f9b0f44d4f79d70bd5afef3725e6b054
theodora-sin <theodrasin@gmail.com>
2026-07-31 07:03:20 +0100

    Update 2Dmap.js

--- 2Dmap.js
+++ 2Dmap.js
@@ -1,6 +1,6 @@
 import{cityData} from "./cityData.js";
 export function load2DMap(){
-    const map=L.map('map').setView9([20,100],3);
+    const map=L.map('map').setView([20,100],3);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom:18,
@@ -8,7 +8,7 @@ export function load2DMap(){
     }).addTo(map);
 
     cityData.forEach(city=> {
-        const marker=L/marker([city.lat, city.lng]).addTo(map);
+        const marker=L.marker([city.lat, city.lng]).addTo(map);
         marker.on("click",()=>{
             showCityInfo(city);
         })
