94ca1693cda7d38b7c961db1690c7e59b0e53857
theodora-sin <theodrasin@gmail.com>
2026-07-31 07:48:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import{ load2DMap }from "./2Dmap.js";
 
 const isMobile= window.innerWidth < 768;
 if(isMobile){
-    document.getElementById("globeViz").style.display="none";
+    document.getElementById("globeWrap").style.display="none";
     document.getElementById("map").style.display="block";
     load2DMap();
 }else{
     document.getElementById("map").style.display = "none";
-    document.getElementById("globeViz").style.display ='block';
+    document.getElementById("globeWrap").style.display = 'block';
     load3DGlobe();
 }
