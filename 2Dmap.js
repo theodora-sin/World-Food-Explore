fbfac95a5bc8bff851eb233bcd27217695e9a97b
theodora-sin <theodrasin@gmail.com>
2026-07-31 09:56:23 +0100

    Update 2Dmap.js

--- 2Dmap.js
+++ 2Dmap.js
@@ -1,13 +1,24 @@
 import{cityData} from "./cityData.js";
 import{renderCityCardHTML, wireCityCardClose} from "./citycard.js"
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',    
+})
+
 export function load2DMap(){
-    const map=L.map('map').setView([20,100],3);
+    const map=L.map('map',{
+        tap:true,
+        tapTolerance:25
+    }).setView([20,100],3);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom:18,
-        attribution:'© OpenStreetMap'
+        attribution:'&copy; OpenStreetMap'
     }).addTo(map);
+
     setTimeout(()=> map.invalidateSize(), 100);
     window.addEventListener('resize',() => map.invalidateSize());
 
@@ -20,7 +31,8 @@ export function load2DMap(){
         marker.on("click",()=>{
             showCityInfoMobile(city);
         })
-    })
+    });
+    return map;
 }
 
 function showCityInfoMobile(city){
