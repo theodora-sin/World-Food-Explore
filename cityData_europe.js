c56654b2363fac39c9c07a9f483de046c3d14417
theodora-sin <theodrasin@gmail.com>
2026-07-31 12:30:36 +0100

    Update cityData_europe.js

--- cityData_europe.js
+++ cityData_europe.js
@@ -1,40 +1,40 @@
-export const cityData_europe=[
- {
-    name:"Athens",
-    country:"Greece",
+export const cityData_europe = [
+  {
+    name: "Athens",
+    country: "Greece",
     lat: 37.977853,
     lng: 23.719602,
-    cuisines:[{
-        type:"Greek",
-        dish_name:"moussaka",
-        course:"Main",
-        photoURL:"images/moussaka/jpg",
-        description:"Layers of soft eggplant, sliced potatoes, and rich minced beef, all topped with a thick, creamy béchamel sauce.",
-        recommendation:[{
-            name:"Filippou Restaurant",
-            price:"$$",
-            address:"Xenokratous 19, Athina 106 75, Greece",
-            link:"https://www.google.com/maps/place/FILIPPOU+Restaurant/@37.9791194,23.7341799,15z/data=!3m1!4b1!4m6!3m5!1s0x14a1bd4612e3b4cb:0x3456a04005ac0b55!8m2!3d37.9791029!4d23.7444796!16s%2Fg%2F1tfh6vck!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D",
-        }]
-  }]
-},
-{
-    name:"Tirana",
-    country:"Albania",
-    lat:41.327694,
-    lng:19.819655,
-    cuisines:[{
-        type:"Albanian",
-        dish_name:"Tavë Kosi",
-        course : "Main",
-        photoURL:"images/tavekosi.webp",
-        description:"Baked lamb and seasoned rice bound in a rich, garlicky yogurt sauce.",
-        recommendation:[{
-            name:"Odas Garden",
-            price:"$$",
-            address:"Rruga Shenasi Dishnica, Tiranë, Albania",
-            link:"https://www.google.com/maps/place/Odas+Garden/@41.3294891,19.8245044,17z/data=!3m1!4b1!4m6!3m5!1s0x135031a4777b5889:0xb87c71c66f2aa4dd!8m2!3d41.3294891!4d19.8245044!16s%2Fg%2F11tnhhznyx!5m1!1e1?entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
-        }]
+    cuisines: [{
+      type: "Greek",
+      dish_name: "moussaka",
+      course: "Main",
+      photoURL: "images/moussaka.jpg",  // also fixed below
+      description: "Layers of soft eggplant, sliced potatoes, and rich minced beef, all topped with a thick, creamy béchamel sauce.",
+      recommendations: [{               // was "recommendation"
+        name: "Filippou Restaurant",
+        priceRange: "$$",               // was "price"
+        address: "Xenokratous 19, Athina 106 75, Greece",
+        link: "https://www.google.com/maps/place/FILIPPOU+Restaurant/@37.9791194,23.7341799,15z"
+      }]
     }]
-},
-]
+  },
+  {
+    name: "Tirana",
+    country: "Albania",
+    lat: 41.327694,
+    lng: 19.819655,
+    cuisines: [{
+      type: "Albanian",
+      dish_name: "Tavë Kosi",
+      course: "Main",
+      photoURL: "images/tavekosi.webp",
+      description: "Baked lamb and seasoned rice bound in a rich, garlicky yogurt sauce.",
+      recommendations: [{               // was "recommendation"
+        name: "Odas Garden",
+        priceRange: "$$",               // was "price"
+        address: "Rruga Shenasi Dishnica, Tiranë, Albania",
+        link: "https://www.google.com/maps/place/Odas+Garden/@41.3294891,19.8245044,17z"
+      }]
+    }]
+  },
+];
