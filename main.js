9921173a85e1f80035b3d7e286df0a03fe285994
theodora-sin <theodrasin@gmail.com>
2026-07-31 06:14:23 +0100

    Add responsive loading of 2D and 3D visualizations

---
+++ main.js
@@ -0,0 +1,13 @@
+import{load3DGlobe} from "./3Dglobe.js";
+import{load2DMap}from "./2Dmap.js";
+
+const isMobile= window.innerWidth < 768;
+if(isMobile){
+    document.getElementById("globeViz").style.display="none";
+    document.getElementById("map").style.display="block";
+    load2DMap();
+}else{
+    document.getElementById("map").style.display = "none";
+    document.getElementsByClassName("globeViz").style.display = 'block';
+    load3DGlobe();
+}
