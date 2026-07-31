8e5044e588c4cfecceb79e9744bae4e21a9701dd
theodora-sin <theodrasin@gmail.com>
2026-07-31 12:55:15 +0100

    Update cityData_europe.js

--- cityData_europe.js
+++ cityData_europe.js
@@ -37,4 +37,100 @@ export const cityData_europe = [
       }]
     }]
   },
+    {
+    name:"London",
+    country:"United Kingdom",
+    lat:51.507167,
+    lng:-0.127996,
+    cuisines:[{
+        type:"Western",
+        dish_name:"fish and chips",
+        course: "Main",
+        photoURL:"images/fish&chips.jpg",
+        description:" Crispy battered white fish served with thick-cut chips, salt, vinegar, and mushy peas. ",
+        recommendations:[{
+            name:"Golden Chippy",
+            priceRange:"$",
+            address:"62 Greenwich High Road, London SE10 8LF England",
+            link:"https://www.google.com/maps/place/Golden+Chippy/@51.4758491,-0.0885637,12z/data=!4m10!1m2!2m1!1sGolden+Chippy!3m6!1s0x4876028bcc3137f5:0xe4e847cf1eb1b32c!8m2!3d51.4758491!4d-0.0185259!15sCg1Hb2xkZW4gQ2hpcHB5Wg8iDWdvbGRlbiBjaGlwcHmSARlmaXNoX2FuZF9jaGlwc19yZXN0YXVyYW504AEA!16s%2Fg%2F1tp1y8lw!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Belfast",
+    country:"United Kingdom",
+    lat:54.597188,
+    lng:-5.930549,
+    cuisines:[{
+        type:"Western",
+        dish_name:"the Ulster Fry",
+        course: "Breakfast",
+        photoURL:"images/ulster_fry.jpg",
+        description:" A heavy cooked breakfast featuring bacon, sausages, eggs, tomatoes, and essential griddle breads like soda bread and potato farls. ",
+        recommendations:[{
+            name:"Maggie May's",
+            priceRange:"$$",
+            address:"9-15 Queen St, Belfast BT1 6EA",
+            link:"https://www.google.com/maps/search/Maggie+May's/@54.5970859,-5.9481244,14.02z/data=!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Lisbon",
+    country:"Portugal",
+    lat:38.721826,
+    lng:-9.139114,
+    cuisines:[{
+        type:"Portuguese ",
+        dish_name:"Bacalhau à Brás",
+        course: "Main",
+        photoURL:"images/bacalhau.jpg",
+        description:" Shredded salted cod mixed with thin-cut fried potatoes, onions, and scrambled eggs, topped with parsley and black olives.",
+        recommendations:[{
+            name:"Laurentina",
+            priceRange:"$$",
+            address:"Av. Conde de Valbom 71A, 1050-067 Lisboa, Portugal",
+            link:"https://www.google.com/maps/place/Laurentina/@38.7371713,-9.1616427,15z/data=!3m1!4b1!4m6!3m5!1s0xd19330c0ac5da71:0x1f87c367e3ca75ca!8m2!3d38.737155!4d-9.151343!16s%2Fg%2F1222j7wb!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Madrid",
+    country:"Spain",
+    lat:40.415748,
+    lng:-3.705628,
+    cuisines:[{
+        type:"Spanish",
+        dish_name:"cocido madrileño",
+        course: "Main",
+        photoURL:"images/cocido.jpg",
+        description:"A rich, slow-cooked chickpea stew packed with meats (chorizo, morcilla, pork belly) and vegetables, traditionally served in distinct courses starting with the soup.",
+        recommendations:[{
+            name:"La Bola ",
+            priceRange:"$$$",
+            address:"C. de la Bola, 5, Centro, 28013 Madrid, Spain",
+            link:"https://www.google.com/maps/place/LA+BOLA/@40.4200269,-3.7207697,15z/data=!3m1!4b1!4m6!3m5!1s0xd42287baca83d53:0x588bdeedd6bda85d!8m2!3d40.420011!4d-3.71047!16s%2Fg%2F120z1rcf!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Barcelona",
+    country:"Spain",
+    lat:41.388308,
+    lng:2.166185,
+    cuisines:[{
+        type:"Spanish",
+        dish_name:"Seafood Paella",
+        course: "Main",
+        photoURL:"images/paella.jpg",
+        description:"Rice cooked in rich fish broth with fresh Mediterranean shrimp, mussels, and squid.",
+        recommendations:[{
+            name:"Can Majó",
+            priceRange:"$$$",
+            address:"Carrer d'Emília Llorca Martín, 23, Ciutat Vella, 08003 Barcelona, Spain",
+            link:"https://www.google.com/maps/place/Can+Maj%C3%B3/@41.3776892,2.1804283,15z/data=!3m1!4b1!4m6!3m5!1s0x12a4a3a90956a6c3:0x1d941415614da50b!8m2!3d41.3776736!4d2.190728!16s%2Fg%2F1tdjdzl3!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDcyOC4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+];
 ];
