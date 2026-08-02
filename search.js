0eee14371061d518c4f6f7b24668ad157e695b4c
theodora-sin <theodrasin@gmail.com>
2026-08-02 09:21:42 +0100

    Fix import path for cityData_america

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 import { cityData } from "./cityData.js";
 import { cityData_europe } from "./cityData_europe.js";
 import { cityData_Africa } from "./cityData_africa.js";
-import { cityData_america} from ".cityData_america.js";
+import { cityData_america} from "./cityData_america.js";
 
 const allCities = [...cityData, ...cityData_europe, ...cityData_Africa];
  
