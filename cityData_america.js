0eddb3495c79f47d4661d687dac5511d1cac2603
theodora-sin <theodrasin@gmail.com>
2026-08-02 09:53:45 +0100

    Update cityData_america.js

--- cityData_america.js
+++ cityData_america.js
@@ -94,4 +94,99 @@ export const cityData_america=[
         }]
     }]
 },
+    {
+    name:"Salt Lake City",
+    country:"USA",
+    lat:40.723784,
+    lng:-111.898521,
+    cuisines:[{
+        type:"Western",
+        dish_name:"Pastrami burger",
+        course: "Main",
+        photoURL:"images/pastrami.jpg",
+        description:"A juicy beef cheeseburger topped with warm, thinly sliced pastrami, lettuce, tomato, and special thousand-island-style burger sauce.",
+        recommendations:[{
+            name:"Crown Burgers",
+            priceRange:"$",
+            address:"118 N 300 W, Salt Lake City, UT 84103, United States",
+            link:"https://www.google.com/maps/search/Crown+Burgers/@40.7110154,-111.9511376,12z?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Houston",
+    country:"USA",
+    lat:29.765244,
+    lng:-95.373972,
+    cuisines:[{
+        type:"Western",
+        dish_name:"beef fajita",
+        course: "Main",
+        photoURL:"images/fajita.jpg",
+        description:"Skirt steak grilled over an open flame and served with warm flour tortillas.",
+        recommendations:[{
+            name:"The Original Ninfa's on Navigation",
+            priceRange:"$$",
+            address:"2704 Navigation Blvd, Houston, TX 77003, United States",
+            link:"https://www.google.com/maps/place/The+Original+Ninfa's+on+Navigation/@29.7569987,-95.3528835,15z/data=!3m1!4b1!4m6!3m5!1s0x8640bedb5bc7e879:0x2fbefdd8fc0214db!8m2!3d29.7569805!4d-95.3426052!16s%2Fg%2F1260t_dg0?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Chicago",
+    country:"USA",
+    lat:41.882849,
+    lng:-87.632538,
+    cuisines:[{
+        type:"Western",
+        dish_name:"Deep-dish pizza",
+        course: "Main",
+        photoURL:"images/deep-dish-pizza.jpg",
+        description:"Baked in a deep, round pan with a high, buttery, golden crust. It features an inverted layer setup: heavy mozzarella cheese at the bottom, meats or vegetables in the middle, and a thick layer of crushed, tangy tomato sauce on top.",
+        recommendations:[{
+            name:"Lou Malnati's Pizzeria",
+            priceRange:"$$",
+            address:"439 N Wells St, Chicago, IL 60654, United States",
+            link:"https://www.google.com/maps/search/Lou+Malnati's+Pizzeria/@41.8903582,-87.6340265,14z?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"Miami",
+    country:"USA",
+    lat:25.761680,
+    lng:-80.198332,
+    cuisines:[{
+        type:"Western",
+        dish_name:"Cuban Sandwich",
+        course: "Main",
+        photoURL:"images/cuban_sandwich.jpg",
+        description:"Layers of roast pork, glazed ham, Swiss cheese, dill pickles, and yellow mustard pressed on a warm, crusty Cuban loaf.",
+        recommendations:[{
+            name:"Sanguich",
+            priceRange:"$$",
+            address:"2057 SW 8th St, Miami, FL 33135, United States",
+            link:"https://www.google.com/maps/place/Sanguich/@25.7657864,-80.2388383,15z/data=!3m2!4b1!5s0x88d9b6fc2f3fc85f:0x7463c2db80904b22!4m6!3m5!1s0x88d9b6fa0e1b0e87:0x1603b809d8c35ab7!8m2!3d25.7657674!4d-80.22856!16s%2Fg%2F11g8gk54b3?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
+{
+    name:"New York",
+    country:"USA",
+    lat:40.714936,
+    lng:-74.008874,
+    cuisines:[{
+        type:"Western",
+        dish_name:"Pastrami on Rye",
+        course: "Main",
+        photoURL:"images/rye.jpg",
+        description:"Cured, smoked, and steamed beef brisket piled high on rye bread with sharp mustard.",
+        recommendations:[{
+            name:"Katz's Delicatessen",
+            priceRange:"$$",
+            address:"205 E Houston St, New York, NY 10002, United States",
+            link:"https://www.google.com/maps/place/Katz's+Delicatessen/@40.7222488,-73.9977073,15z/data=!3m1!4b1!4m6!3m5!1s0x89c2598f7ff4aa09:0x313547e757cb8cea!8m2!3d40.722233!4d-73.987429!16zL20vMDN0eF9o?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D"
+        }]
+    }]
+},
 ]
